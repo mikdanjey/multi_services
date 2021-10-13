f5d501bbeb0abfde99b06d28add1751cf4916413
Manikandan A <mikdanjey@gmail.com>
2021-10-14 02:34:30 +0530

    ok

--- ui-analytics/src/App.js
+++ ui-analytics/src/App.js
@@ -90,28 +90,26 @@ class App extends Component {
 
         <tr>
           <th>All Data <button onClick={this.getDruidAllData}>Get Data</button></th>
+          <th>Last Month Data <button onClick={this.getDruidLastMonthData}>Get Data</button></th>
         </tr>
         <tr>
-          <td>
+
+          <td width="280px">
             <code>
               <pre>
                 {jsonAllData}
               </pre>
             </code>
           </td>
-        </tr>
 
-        <tr>
-          <th>Last Month Data <button onClick={this.getDruidAllData}>Get Data</button></th>
-        </tr>
-        <tr>
-          <td>
+          <td width="280px">
             <code>
               <pre>
                 {jsonLastMonthData}
               </pre>
             </code>
           </td>
+
         </tr>
 
       </table>
