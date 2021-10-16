fe3d0b11c0a701a2b699ebe7c22efb6e623062ae
Manikandan A <mikdanjey@gmail.com>
2021-10-16 19:19:26 +0530

    ok

--- ui-analytics/src/App.js
+++ ui-analytics/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import { Button, Container, Row, Col, Card } from 'react-bootstrap';
+import { JsonToTable } from "react-json-to-table";
 class App extends Component {
 
   constructor(props) {
@@ -12,8 +14,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getDruidAllData();
-    this.getDruidLastMonthData();
+    // this.getDruidAllData();
+    // this.getDruidLastMonthData();
   }
 
   getDruidAllData = () => {
@@ -34,10 +36,10 @@ class App extends Component {
     const thisClass = this;
     axios.post(this.baseURL,
       {
-        query: "SELECT COUNT(*) AS TotalRecords FROM transactions"
+        query: "SELECT amount, channel, customerId, deviceType,  ownerId, tpn, transactionDate FROM transactions LIMIT 10"
       })
       .then(response => {
-        thisClass.setState({ jsonLastMonthData: JSON.stringify(response.data, null, 2) });
+        thisClass.setState({ jsonLastMonthData: response.data });
       })
       .catch(error => {
         console.error('There was an error!', error);
@@ -46,35 +48,35 @@ class App extends Component {
 
   render() {
     const { jsonAllData, jsonLastMonthData } = this.state;
-    return <div>
-      <table border="2" cellSpacing="0" cellPadding="0">
-        <thead>
-          <tr>
-            <th>All Data <button onClick={this.getDruidAllData}>Get Data</button></th>
-            <th>Last Month Data <button onClick={this.getDruidLastMonthData}>Get Data</button></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td width="280px">
-              <code>
-                <pre>
-                  {jsonAllData}
-                </pre>
-              </code>
-            </td>
-
-            <td width="280px">
-              <code>
-                <pre>
-                  {jsonLastMonthData}
-                </pre>
-              </code>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>;
+    return (
+      <Container fluid>
+        <br />
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header as="h5">All Data <Button onClick={this.getDruidAllData} variant="primary">Get Data</Button></Card.Header>
+              <Card.Body>
+                <code>
+                  <pre>
+                    {jsonAllData}
+                  </pre>
+                </code>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header as="h5">Last Month Data <Button onClick={this.getDruidLastMonthData} variant="primary">Get Data</Button></Card.Header>
+              <Card.Body>
+                <JsonToTable json={jsonLastMonthData} />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
 
