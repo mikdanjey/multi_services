a0fa9d684a01de9129850dc582f397cf37591e59
Manikandan A <mikdanjey@gmail.com>
2021-10-17 00:36:03 +0530

    ok

--- ui-analytics/src/App.js
+++ ui-analytics/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import { Button, Container, Row, Col, Card } from 'react-bootstrap';
+import { Button, Container, Row, Col, Card, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { JsonToTable } from "react-json-to-table";
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      jsonAllData: null,
-      jsonLastMonthData: null,
+      jsonAllData: [],
+      jsonLastMonthData: [],
     }
     this.baseURL = `/druid/v2/sql`; // ${window.location.host}
   }
@@ -36,7 +36,7 @@ class App extends Component {
     const thisClass = this;
     axios.post(this.baseURL,
       {
-        query: "SELECT amount, channel, customerId, deviceType,  ownerId, tpn, transactionDate FROM transactions LIMIT 10"
+        query: "SELECT amount, channel, customerId, deviceType,  ownerId, tpn, transactionDate FROM transactions LIMIT 100"
       })
       .then(response => {
         thisClass.setState({ jsonLastMonthData: response.data });
@@ -49,33 +49,47 @@ class App extends Component {
   render() {
     const { jsonAllData, jsonLastMonthData } = this.state;
     return (
-      <Container fluid>
-        <br />
-        <Row>
-          <Col>
-            <Card>
-              <Card.Header as="h5">All Data <Button onClick={this.getDruidAllData} variant="primary">Get Data</Button></Card.Header>
-              <Card.Body>
-                <code>
-                  <pre>
-                    {jsonAllData}
-                  </pre>
-                </code>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Header as="h5">Last Month Data <Button onClick={this.getDruidLastMonthData} variant="primary">Get Data</Button></Card.Header>
-              <Card.Body>
-                <JsonToTable json={jsonLastMonthData} />
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-          </Col>
-        </Row>
-      </Container>
+      <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" >
+          <Container>
+            <Navbar.Brand href="/">Analytics</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container fluid>
+          <br />
+          <Row>
+            <Col>
+              <Card>
+                <Card.Header as="h5">All Data <Button onClick={this.getDruidAllData} variant="primary">Get Data</Button></Card.Header>
+                <Card.Body>
+                  <code>
+                    <pre>
+                      {jsonAllData}
+                    </pre>
+                  </code>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Header as="h5">Last Month Data <Button onClick={this.getDruidLastMonthData} variant="primary">Get Data</Button></Card.Header>
+                <Card.Body>
+                  <JsonToTable json={jsonLastMonthData} />
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+            </Col>
+          </Row>
+        </Container>
+      </>
     )
   }
 }
