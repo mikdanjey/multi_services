6cc0227a6adaa7946184bbbd59cd03b976f23e95
Manikandan A <mikdanjey@gmail.com>
2021-10-16 18:38:26 +0530

    ok

--- ui-analytics/src/App.js
+++ ui-analytics/src/App.js
@@ -1,31 +1,14 @@
 import React, { Component } from "react";
-import plywood from "plywood";
-
-var External = plywood.External;
-var druidRequesterFactory = require('plywood-druid-requester').druidRequesterFactory;
-
+import axios from 'axios';
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.ply = plywood.ply;
-    this.$ = plywood.$;
     this.state = {
       jsonAllData: null,
       jsonLastMonthData: null,
     }
-    this.druidRequester = druidRequesterFactory({
-      host: window.location.host // Where ever your Druid may be
-    });
-
-    this.druidDataset = External.fromJS({
-      engine: 'druid',
-      source: 'transactions',  // The datasource name in Druid
-      timeAttribute: 'time',  // Druid's anonymous time attribute will be called 'time',
-      context: {
-        timeout: 10000 // The Druid context
-      }
-    }, this.druidRequester);
+    this.baseURL = `/druid/v2/sql`; // ${window.location.host}
   }
 
   componentDidMount() {
@@ -35,83 +18,61 @@ class App extends Component {
 
   getDruidAllData = () => {
     const thisClass = this;
-    var context = {
-      transactions: this.druidDataset
-    };
-    var ex = this.ply()
-      // Define the external in scope with a filter on time and language
-      .apply("transactions",
-        this.$('transactions').filter(this.$("transactionDate").in({
-          start: new Date("2020-01-01T00:00:00Z"),
-          end: new Date("2021-10-01T00:00:00Z")
-        }))
-      )
-      // Calculate count
-      .apply('count', this.$('transactions').count())
-      // Calculate the sum of the `added` attribute
-      .apply('amount', '$transactions.sum($amount)');
-    ex.compute(context).then(function (data) {
-      // Log the data while converting it to a readable standard
-      // console.log(JSON.stringify(data.toJS(), null, 2));
-      thisClass.setState({ jsonAllData: JSON.stringify(data.toJS(), null, 2) });
-    });
-    // .done();
+    axios.post(this.baseURL,
+      {
+        query: "SELECT COUNT(*) AS TotalRecords FROM transactions"
+      })
+      .then(response => {
+        thisClass.setState({ jsonAllData: JSON.stringify(response.data, null, 2) });
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
   }
 
   getDruidLastMonthData = () => {
     const thisClass = this;
-    var context = {
-      transactions: this.druidDataset
-    };
-    var ex = this.ply()
-      // Define the external in scope with a filter on time and language
-      .apply("transactions",
-        this.$('transactions').filter(this.$("transactionDate").in({
-          start: new Date("2021-09-01T00:00:00Z"),
-          end: new Date("2021-09-30T00:00:00Z")
-        }))
-      )
-      // Calculate count
-      .apply('count', this.$('transactions').count())
-      // Calculate the sum of the `added` attribute
-      .apply('amount', '$transactions.sum($amount)');
-    ex.compute(context).then(function (data) {
-      // Log the data while converting it to a readable standard
-      // console.log(JSON.stringify(data.toJS(), null, 2));
-      thisClass.setState({ jsonLastMonthData: JSON.stringify(data.toJS(), null, 2) });
-    });
-    // .done();
+    axios.post(this.baseURL,
+      {
+        query: "SELECT COUNT(*) AS TotalRecords FROM transactions"
+      })
+      .then(response => {
+        thisClass.setState({ jsonLastMonthData: JSON.stringify(response.data, null, 2) });
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
   }
 
   render() {
     const { jsonAllData, jsonLastMonthData } = this.state;
     return <div>
-      <table border="2" cellspacing="0" cellpadding="0">
+      <table border="2" cellSpacing="0" cellPadding="0">
+        <thead>
+          <tr>
+            <th>All Data <button onClick={this.getDruidAllData}>Get Data</button></th>
+            <th>Last Month Data <button onClick={this.getDruidLastMonthData}>Get Data</button></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td width="280px">
+              <code>
+                <pre>
+                  {jsonAllData}
+                </pre>
+              </code>
+            </td>
 
-        <tr>
-          <th>All Data <button onClick={this.getDruidAllData}>Get Data</button></th>
-          <th>Last Month Data <button onClick={this.getDruidLastMonthData}>Get Data</button></th>
-        </tr>
-        <tr>
-
-          <td width="280px">
-            <code>
-              <pre>
-                {jsonAllData}
-              </pre>
-            </code>
-          </td>
-
-          <td width="280px">
-            <code>
-              <pre>
-                {jsonLastMonthData}
-              </pre>
-            </code>
-          </td>
-
-        </tr>
-
+            <td width="280px">
+              <code>
+                <pre>
+                  {jsonLastMonthData}
+                </pre>
+              </code>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>;
   }
