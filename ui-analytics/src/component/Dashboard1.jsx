d1c699da195abd959fc76f2f1df9b02a88c10828
Manikandan A <mikdanjey@gmail.com>
2021-10-17 18:37:57 +0530

    Update Dashboard1.jsx

--- ui-analytics/src/component/Dashboard1.jsx
+++ ui-analytics/src/component/Dashboard1.jsx
@@ -23,6 +23,9 @@ class Dashboard1 extends Component {
 
       widgetData5: [],
       isWidgetLoaderVisible5: true,
+
+      widgetData6: [],
+      isWidgetLoaderVisible6: true,
     }
     this.baseURL = `/druid/v2/sql`;
 
@@ -31,11 +34,11 @@ class Dashboard1 extends Component {
   sleeper = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
   componentDidMount() {
-    this.getDruidWidget1();
-    this.getDruidWidget2();
-    this.getDruidWidget3();
-    this.getDruidWidget4();
-    this.getDruidWidget5();
+    // this.getDruidWidget1();
+    // this.getDruidWidget2();
+    // this.getDruidWidget3();
+    // this.getDruidWidget4();
+    // this.getDruidWidget5();
 
   }
 
@@ -163,12 +166,61 @@ class Dashboard1 extends Component {
     }
   }
 
+  getDruidWidget6 = async () => {
+    const thisClass = this;
+    thisClass.setState({ isWidgetLoaderVisible6: true });
+    // await thisClass.sleeper(100);
+    let response = [];
+    let widgetData6 = [];
+    try {
+      response = await axios.post(this.baseURL, {
+        query: `
+        SELECT COUNT(*) as "Total Count", ROUND(SUM(amount), 0) as "Total Volume"
+        FROM transactions WHERE __time BETWEEN '2020-01-01' AND '2021-12-31'
+        `
+      });
+      widgetData6 = response.data;
+      thisClass.setState({ isWidgetLoaderVisible6: false, widgetData6 });
+    } catch (error) {
+      thisClass.setState({ isWidgetLoaderVisible6: true });
+      console.error('There was an error!', error);
+    }
+  }
+
   render() {
-    const { widgetData1, isWidgetLoaderVisible1, widgetData2, isWidgetLoaderVisible2, widgetData3, isWidgetLoaderVisible3, widgetData4, isWidgetLoaderVisible4, widgetData5, isWidgetLoaderVisible5, } = this.state;
+    const { widgetData1, isWidgetLoaderVisible1, widgetData2, isWidgetLoaderVisible2, widgetData3, isWidgetLoaderVisible3, widgetData4, isWidgetLoaderVisible4, widgetData5, isWidgetLoaderVisible5, widgetData6, isWidgetLoaderVisible6, } = this.state;
     return (
       <>
         <Container fluid style={{ padding: 20 }}>
           <Row>
+
+          <Col>
+              <Card>
+                <Card.Header>
+                  <div className="row">
+                    <div className="col">
+                      <h5 className="card-title">By Overall Data</h5>
+                    </div>
+                    <div className="col text-end">
+                      <Button onClick={this.getDruidWidget6} variant="primary">Get Data</Button>
+                    </div>
+                  </div>
+                </Card.Header>
+                <Card.Body>
+                  <Loader
+                    type="Circles"
+                    color="#50C878"
+                    height={100}
+                    width={100}
+                    visible={isWidgetLoaderVisible6}
+                  />
+                  {!isWidgetLoaderVisible6 &&
+                    <JsonToTable json={widgetData6} />
+                  }
+                </Card.Body>
+              </Card>
+            </Col>
+
             <Col>
               <Card>
                 <Card.Header>
@@ -223,6 +275,40 @@ class Dashboard1 extends Component {
               </Card>
             </Col>
 
+            
+
+          </Row>
+          <br />
+          <Row>
+
+
+            <Col>
+              <Card>
+                <Card.Header>
+                  <div className="row">
+                    <div className="col">
+                      <h5 className="card-title">By Device Type Data</h5>
+                    </div>
+                    <div className="col text-end">
+                      <Button onClick={this.getDruidWidget5} variant="primary">Get Data</Button>
+                    </div>
+                  </div>
+                </Card.Header>
+                <Card.Body>
+                  <Loader
+                    type="Circles"
+                    color="#50C878"
+                    height={100}
+                    width={100}
+                    visible={isWidgetLoaderVisible5}
+                  />
+                  {!isWidgetLoaderVisible5 &&
+                    <JsonToTable json={widgetData5} />
+                  }
+                </Card.Body>
+              </Card>
+            </Col>
+
             <Col>
               <Card>
                 <Card.Header>
@@ -250,38 +336,6 @@ class Dashboard1 extends Component {
               </Card>
             </Col>
 
-          </Row>
-          <br />
-          <Row>
-
-            
-            <Col>
-              <Card>
-                <Card.Header>
-                  <div className="row">
-                    <div className="col">
-                      <h5 className="card-title">By Device Type Data</h5>
-                    </div>
-                    <div className="col text-end">
-                      <Button onClick={this.getDruidWidget5} variant="primary">Get Data</Button>
-                    </div>
-                  </div>
-                </Card.Header>
-                <Card.Body>
-                  <Loader
-                    type="Circles"
-                    color="#50C878"
-                    height={100}
-                    width={100}
-                    visible={isWidgetLoaderVisible5}
-                  />
-                  {!isWidgetLoaderVisible5 &&
-                    <JsonToTable json={widgetData5} />
-                  }
-                </Card.Body>
-              </Card>
-            </Col>
-            
             <Col>
               <Card>
                 <Card.Header>
@@ -308,7 +362,6 @@ class Dashboard1 extends Component {
                 </Card.Body>
               </Card>
             </Col>
-            <Col></Col>
           </Row>
 
         </Container>
