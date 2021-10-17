51b8cb193bc92f856d660ad601dbb8d0bd08a27f
Manikandan A <mikdanjey@gmail.com>
2021-10-17 12:37:34 +0530

    ok

--- kafka/config.sample.js
+++ kafka/config.sample.js
@@ -1,12 +1,12 @@
 let config = {};
 
 config.CLOUD_KAFKA_TOPIC_PREFIX = "";
-config.CLOUD_KAFKA_TOPIC = `${config.CLOUD_KAFKA_TOPIC_PREFIX}`;
-config.CLOUD_KAFKA_MAX_PARTITION = 3;
-config.CLOUD_KAFKA_BROKERS = ["localhost:9094",];
+config.CLOUD_KAFKA_TOPIC = `${config.CLOUD_KAFKA_TOPIC_PREFIX}transactions`;
+config.CLOUD_KAFKA_BROKERS = ["localhost:9094"];
 config.CLOUD_KAFKA_USERNAME = "";
 config.CLOUD_KAFKA_PASSWORD = "";
-config.CLOUD_KAFKA_CLIENT_ID = "users-app";
+config.CLOUD_KAFKA_CLIENT_ID = "my-app";
 config.CLOUD_KAFKA_GROUP_ID = "users";
+config.CLOUD_KAFKA_MAX_PARTITION = 8;
 
 module.exports = config;
